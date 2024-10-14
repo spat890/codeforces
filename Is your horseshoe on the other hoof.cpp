d5dac79d0ac31c8aab19cea908920befc8af9acb
spat890 <saeepatil890@gmail.com>
2024-10-15 00:04:43 +0530

    Add files via upload

---
+++ Is your horseshoe on the other hoof.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int s1,s2,s3,s4;
+	cin>>s1>>s2>>s3>>s4;
+	int arr[]={s1,s2,s3,s4};
+	int c=0;
+	
+	for(int i=0;i<4;i++)
+	{
+	    sort(arr,arr+4);
+
+		if(arr[i]!=arr[i+1])
+		{
+			c++;
+		}
+	}
+	cout<<4-c;
+	return 0;
+}
