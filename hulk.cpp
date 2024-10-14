caed708f14ba8c08c8e2a15f5fb09f0125a786d1
spat890 <saeepatil890@gmail.com>
2024-10-15 00:06:19 +0530

    Add files via upload

---
+++ hulk.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main() 
+	{
+ 		int n;
+ 		cin>>n;
+ 		string s="";
+ 
+ 	for(int i=0;i<n;i++)
+		{
+ 	 		if(i%2==0&&i!=n-1)
+			{
+ 	 			s+="I hate that ";
+ 	 		}
+ 	 		else if(i%2==1&&i!=n-1)
+	    	{
+ 	 			s+="I love that ";
+ 	 		}
+ 	 		else if(i%2==0)
+	    	{
+ 	 			s+="I hate ";
+ 	    	}
+ 	 	
+ 	 		else if(i%2==1)
+	    	{
+ 	 			s+="I love ";
+ 	    	}
+  
+ 		}
+ 		
+ s+="it";
+ cout<<s<<endl;
+ 	return 0;
+}
