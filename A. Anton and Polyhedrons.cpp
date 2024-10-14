8e35a7810cbc4d4ea9494c690e81e4d0effb82ef
spat890 <saeepatil890@gmail.com>
2024-10-15 00:20:09 +0530

    Add files via upload

---
+++ A. Anton and Polyhedrons.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	string s[n];
+	int c=0;
+	
+		for(int i=0;i<n;i++)
+	{
+		cin>>s[i];
+    } 
+	for(int i=0;i<n;i++)
+	{
+		if(s[i]=="Tetrahedron")
+		{
+			c=c+4;
+		}
+		else if(s[i]=="Cube")
+		{
+			c=c+6;
+		}
+		else if(s[i]=="Octahedron")
+		{
+			c=c+8;
+		}
+		else if(s[i]=="Dodecahedron")
+		{
+			c=c+12;
+		}
+		else if(s[i]=="Icosahedron")
+		{
+			c=c+20;
+		}
+	}
+	
+	cout<<c<<endl;
+	return 0;
+}
